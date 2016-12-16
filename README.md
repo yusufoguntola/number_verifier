2df214932d7b641df524e197dcc429add4ba775d
abumumtaaz <yooguntola@student.lautech.edu.ng>
2016-12-16 11:09:35 +0100

    Made some home keep

---
+++ README.md
@@ -0,0 +1,7 @@
+Nigerian Vehicle Plate Number Verifier
+
+This library enables you verify plate number (of Nigerian vehicles) ang get the details
+of the vehicle and the verified owner.
+
+
+This library is licensed under the MIT License. Please take a look at it!
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='number_verifier',
+    packages=['number_verifier'],
+    version='0.1',
+    description='Nigerian Vehicle Plate Number Verifier lib',
+    author='Yusuf Oguntola',
+    author_email='yusuf.oguntola@gmail.com',
+    url='https://github.com/yusufoguntola/number_verifier',
+    download_url='https://github.com/yusufoguntola/number_verifier/tarball/0.1',
+    keywords=['plate-number', 'Nigeria', 'verify', 'vehicle'],
+    classifiers=[],
+)
